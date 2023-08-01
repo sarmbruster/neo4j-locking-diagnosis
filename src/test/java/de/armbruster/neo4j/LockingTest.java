ef850fe63e83948661991a8cef015484e041ceae
Stefan Armbruster <stefan@armbruster-it.de>
2023-08-01 10:31:40 +0200

    test code

--- src/test/java/de/armbruster/neo4j/LockingTest.java
+++ src/test/java/de/armbruster/neo4j/LockingTest.java
@@ -1,8 +1,29 @@
 package de.armbruster.neo4j;
 
+import org.junit.Test;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
-class LockingTest {
+public class LockingTest {
 
+    @Container
+    private static Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>(DockerImageName.parse("neo4j:4.4.19"))
+            //.withEnterpriseEdition()
+            .withoutAuthentication();
+
+    @Test
+    public void testLocking() {
+        //try (Neo4jContainer<?> neo4jContainer= new Neo4jContainer<>(DockerImageName.parse("neo4j:4.4.19")).withoutAuthentication().withEnterpriseEdition()) {
+            //neo4jContainer.start();
+            try (Driver driver = GraphDatabase.driver(neo4jContainer.getBoltUrl(), AuthTokens.none())) {
+                System.out.println("HURZ");
+            }
+        //}
+    }
 }
